b4b42b1a34efd598723c5f9d4ea0208747017d2f
Matheus Nunes <matheus.o.nunes@gmail.com>
2020-09-12 17:15:51 -0300

    Skip compression for images

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const {gzip} = require('node-gzip');
 const zlib = require('zlib')
-const request = require('request')
+const request = require('request').defaults({ encoding: null });
 
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
   const host = process.env.FILE_HOST
   const accept_encoding = event.headers['accept-encoding']
   const accept_brotli = accept_encoding && accept_encoding.indexOf('br') !== -1
   const accept_gzip = accept_encoding && accept_encoding.indexOf('gzip') !== -1
 
-  const path = event.rawPath.replace(process.env.API_GATEWAY_PATH, '')
+  const path = event.path.replace(process.env.API_GATEWAY_PATH, '')
 
-  const request_promise = new Promise((resolve, reject) => {
-    request(`${host}${path}`, function (error, response, body) {
-      if(response.statusCode != 200) { // Return fallback route
-        return request(`${host}/not_found`, function (error, response, body) {
-          resolve({ body: body, headers: response.headers })
-        })
-      }
+  const file_extension = /.+\.([a-z]+)$/.exec(path) && /.+\.([a-z]+)$/.exec(path)[1]
 
-      resolve({ body: body, headers: response.headers })
-    })
-  })
-
-
-  const host_response = await request_promise
+  const host_response = await download(`${host}${path}`)
   let body = host_response.body
+
+  if(file_extension && !['css', 'js'].includes(file_extension)) {
+    buffer = Buffer.from(body)
+    const response = {
+      statusCode: 200,
+      body: buffer.toString('base64'),
+      isBase64Encoded: true,
+      headers: {
+        'Content-Type': host_response.headers["content-type"]
+      }
+    }
+    return response
+  }
+
   let content_encoding
   if(accept_brotli) {
     content_encoding = "br"
@@ -51,3 +54,12 @@ exports.handler = async (event) => {
   };
   return response;
 };
+
+function download(uri) {
+  return new Promise((resolve, reject) => {
+    request.get(uri, function (error, response, body) {
+      // data = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
+      resolve({ body: body, headers: response.headers })
+    })
+  })
+}
